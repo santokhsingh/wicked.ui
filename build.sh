9cb4acb9b9ea66e3857aaad3ab146fabaff6c4af
Martin Danielsson <martin@danielsson.de>
2017-04-02 16:55:31 +0200

    Alpine build preparations, added build script

---
+++ build.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+set -e
+
+if [ -z "$DOCKER_PREFIX" ]; then
+    echo "WARNING: Env var DOCKER_PREFIX not set, assuming haufelexware/wicked."
+    export DOCKER_PREFIX="haufelexware/wicked."
+fi
+
+if [ -z "$DOCKER_TAG" ]; then
+    echo "WARNING: Env var DOCKER_TAG is not set, assuming 'dev'."
+    export DOCKER_TAG=dev
+fi
+
+echo "============================================"
+echo "Building normal image..."
+echo "============================================"
+
+export BUILD_ALPINE=""
+perl -pe 's;(\\*)(\$([a-zA-Z_][a-zA-Z_0-9]*)|\$\{([a-zA-Z_][a-zA-Z_0-9]*)\})?;substr($1,0,int(length($1)/2)).($2&&length($1)%2?$2:$ENV{$3||$4});eg' Dockerfile.template > Dockerfile
+docker build -t ${DOCKER_PREFIX}portal:${DOCKER_TAG} .
+
+echo "============================================"
+echo "Building alpine image..."
+echo "============================================"
+
+export BUILD_ALPINE="-alpine"
+perl -pe 's;(\\*)(\$([a-zA-Z_][a-zA-Z_0-9]*)|\$\{([a-zA-Z_][a-zA-Z_0-9]*)\})?;substr($1,0,int(length($1)/2)).($2&&length($1)%2?$2:$ENV{$3||$4});eg' Dockerfile.template > Dockerfile-alpine
+docker build -f Dockerfile-alpine -t ${DOCKER_PREFIX}portal:${DOCKER_TAG}-alpine .
+
+if [ "$1" = "--push" ]; then
+    echo "============================================"
+    echo "Logging in to registry..."
+    echo "============================================"
+
+    if [ -z "$DOCKER_REGISTRY_USER" ] || [ -z "$DOCKER_REGISTRY_PASSWORD" ]; then
+        echo "ERROR: Env vars DOCKER_REGISTRY_USER and/or DOCKER_REGISTRY_PASSWORD not set."
+        echo "Cannot push images, exiting."
+        exit 1
+    fi
+
+    if [ -z "$DOCKER_REGISTRY" ]; then
+        echo "WARNING: Env var DOCKER_REGISTRY not set, assuming official docker hub."
+        docker login -u ${DOCKER_REGISTRY_USER} -p ${DOCKER_REGISTRY_PASSWORD}
+    else
+        docker login -u ${DOCKER_REGISTRY_USER} -p ${DOCKER_REGISTRY_PASSWORD} ${DOCKER_REGISTRY}
+    fi
+
+    echo "============================================"
+    echo "Pushing ${DOCKER_PREFIX}portal:${DOCKER_TAG}-onbuild"
+    echo "============================================"
+
+    docker push ${DOCKER_PREFIX}portal:${DOCKER_TAG}-onbuild
+
+    echo "============================================"
+    echo "Pushing ${DOCKER_PREFIX}portal:${DOCKER_TAG}-onbuild-alpine"
+    echo "============================================"
+    
+    docker push ${DOCKER_PREFIX}portal:${DOCKER_TAG}-onbuild-alpine
+else
+    if [ ! -z "$1" ]; then
+        echo "WARNING: Unknown parameter '$1'; did you mean --push?"
+    fi
+fi
