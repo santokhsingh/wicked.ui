9bc97e184dd6a6db396cd12a5668469ecb1bde30
Santokh Singh <santokh.singh@thomsonreuters.com>
2017-10-28 17:00:09 -0700

    Fix custom entity  issues

---
+++ routes/clarivate.js
@@ -0,0 +1,128 @@
+'use strict';
+var request = require('request');
+var express = require('express');
+var router = express.Router();
+var async = require('async');
+var debug = require('debug')('portal:clarivate');
+var tmp = require('tmp');
+var fs = require('fs');
+var util = require('util');
+var utils = require('./utils');
+
+
+router.get('/subscriptions', function (req, res, next) {
+    debug("get('/subscriptions')");
+    // This is not super good; this is expensive. Lots of calls.
+    utils.getFromAsync(req, res, '/applications', 200, function (err, appsResponse) {
+        if (err)
+            return next(err);
+        var appIds = [];
+        for (var i = 0; i < appsResponse.length; ++i)
+            appIds.push(appsResponse[i].id);
+      
+        // This is the expensive part:
+        async.map(appIds, function (appId, callback) {
+            utils.getFromAsync(req, res, '/applications/' + appId +'/subscriptions', 200, callback);
+        }, function (err, appsSubscInfos) {
+            if (err)
+                return next(err);
+            var subs=[];
+            for (var i = 0; i < appsSubscInfos.length; ++i) {
+                var sub = appsSubscInfos[i];
+                 for (var j = 0; j < sub.length; ++j){
+                  subs.push(sub[j]);
+                } 
+            }
+            res.json({
+              title: 'All Subsriptions',
+              subscriptions: subs
+            });   
+        });
+    });  
+
+});
+
+router.post('/customheaders/:pluginId', function (req, res, next) {  
+  var body = utils.getJson(req.body);
+  var pluginId = req.params.pluginId;
+  var key = body.key;
+  var apiId = body.api;
+  var headers = body.headers;
+  var pdata =  utils.getJson(body.pdata);
+  var data=[];
+  for (var i = 0; i < pdata.headers.length; ++i) {
+    if(pdata.headers[i].key===key){
+       pdata.headers[i].headers = utils.getJson(headers);
+    }
+    data.push(pdata.headers[i]);
+  }
+  if(data.length<=0){
+    data = {"key": key, "headers":  headers}
+  }
+  var myObject = {};
+  var params = [];
+  myObject["name"]= "custom-key-headers";
+  myObject["config"] = {}
+  for (var i = 0; i < data.length; ++i) {
+    params.push(data[i]);
+  }
+  myObject["config"]["parameters"]=params;
+  patchAdmin(req, res, '/apis/'+apiId+'/plugins/'+pluginId, myObject, function (err, pluginsResponse) {
+    if (err)
+       return next(err);
+    res.json(pluginsResponse);
+  });
+});
+
+function getBaseUri(req){
+  var baseUrl = "http://localhost:8001";
+  if(req.app.portalGlobals.network.kongAdminUrl){
+      baseUrl = req.app.portalGlobals.network.kongAdminUrl;
+  }
+  return baseUrl;
+}
+
+function patchAdmin(req, res, uri, data, callback) {
+   var baseUrl =  getBaseUri(req);
+   request.patch(
+        {
+            
+            url: baseUrl + uri,
+            body: data,
+            json: true,
+        },
+        callback);
+}
+
+function getAdmin(req, res, uri, callback) {
+    var baseUrl =  getBaseUri(req);
+    request.get(
+        { 
+            url: baseUrl + uri
+        },
+        callback);
+};
+
+router.get('/customheaders/:apiId', function (req, res, next) {
+  var apiId = req.params.apiId;
+  var payload=[];
+  getAdmin(req, res, '/apis/'+apiId+'/plugins', function (err, pluginsResponse) {
+     if (err)
+       return next(err);
+     var body = utils.getJson(pluginsResponse.body);
+     var pid;
+     for (var i = 0; i < body.data.length; ++i) {
+        var plugin_name = body.data[i].name;
+        if(plugin_name === 'custom-key-headers'){
+          var params = body.data[i].config.parameters;
+          pid = body.data[i].id;
+          for (var j = 0; j < params.length; ++j){
+            payload.push(utils.getJson(params[j]));
+          }
+        }
+     }
+     res.json({ headers: payload, pluginid:  pid});    
+  });
+});   
+
+module.exports = router;
